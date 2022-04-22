3f86ba83f8088c4663838972fba30ae9e1b0ea3c
sjhuang <hsjfans_scholar@163.com>
2022-04-22 15:22:25 +0800

    add mmap test

--- db/nosdb/file/file_handle_test.go
+++ db/nosdb/file/file_handle_test.go
@@ -42,3 +42,40 @@ func TestIOFileHandle_ReadAt(t *testing.T) {
 	}
 	fmt.Println(string(b))
 }
+
+func TestMMapFileHandle_WriteAt(t *testing.T) {
+	file, err := os.OpenFile("test.txt", os.O_CREATE|os.O_RDWR, 0664)
+	if err != nil {
+		t.Error(err)
+	}
+
+	handle, err := NewFileHandle(M_MAP, file, -1)
+	if err != nil {
+		t.Error(err)
+	}
+	defer handle.Close()
+	value := []byte("hsj is a bed man. and more time 杀人诛心!")
+	offset, err := handle.WriteAt(1024, value)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(offset)
+}
+
+func TestMMapFileHandle_ReadAt(t *testing.T) {
+	file, err := os.OpenFile("test.txt", os.O_CREATE|os.O_RDWR, 0664)
+	if err != nil {
+		t.Error(err)
+	}
+
+	handle, err := NewFileHandle(M_MAP, file, -1)
+	if err != nil {
+		t.Error(err)
+	}
+	defer handle.Close()
+	b, err := handle.ReadAt(1024, 45)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(string(b))
+}
